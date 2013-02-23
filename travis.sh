ddfe443f201116fa5a8c945e63139d22039c10a2
Ivan Borzenkov <ivan1986@list.ru>
2013-02-24 00:36:10 +0400

    script

--- travis.sh
+++ travis.sh
@@ -1,7 +1,8 @@
-!/bin/sh
-rm -rf .git
-git clone --depth=10 --quiet git://github.com/psi-im/psi .
+#!/bin/sh
+git clone --depth=10 --quiet git://github.com/psi-im/psi psi || exit 1
+cd psi
 git submodule init
 git submodule update
+ln -s ../patches .
 ls -la
 
