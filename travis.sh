b93c8463d0ab2a03e1f53881e102ea07748f96f6
Ivan Borzenkov <ivan1986@list.ru>
2013-02-24 00:47:22 +0400

    all script

--- travis.sh
+++ travis.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 git clone --depth=10 --quiet git://github.com/psi-im/psi psi || exit 1
 cd psi
-git submodule init
-git submodule update
+git submodule update --init --recursive || exit 1
 ln -s ../patches .
-ls -la
+cd patches
+ls -1 *.diff > series
+cd ..
+quilt push -a || exit 1
+qt-qconf || exit 1
+./configure --enable-plugins --enable-whiteboarding --debug --no-separate-debug-info || exit 1
+make || exit 1
 
